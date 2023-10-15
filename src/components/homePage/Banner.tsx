be9757736fc20d2c50fa1c25b6739e4e79d2c464
AmtTawsik <amttawsik.cse@gmail.com>
2023-10-15 14:44:00 +0600

    Add images in slider

--- src/components/homePage/Banner.tsx
+++ src/components/homePage/Banner.tsx
@@ -2,7 +2,7 @@ import { Carousel } from 'antd';
 import React from 'react';
 
 const contentStyle: React.CSSProperties = {
-  height: '500px',
+  height: '600px',
   color: '#fff',
   lineHeight: '160px',
   textAlign: 'center',
@@ -11,16 +11,17 @@ const contentStyle: React.CSSProperties = {
 
 const HomeBanner = () => (
   <Carousel autoplay>
-    <div className=" bg-teal-700 h-[500px]">
-      <h3>Hello house crafters</h3>
+    <div className=" bg-teal-700 md:max-h-[83.5vh]">
+        <img className='w-full' src="https://i.ibb.co/0GHJ55Y/slider1.webp" alt="Slider-1" />
     </div>
-    <div className=" bg-teal-700 h-[500px]">
-      <h3>Hello house crafters</h3>
+    <div className=" bg-teal-700 md:max-h-[83.5vh]">
+        <img className='w-full' src="https://i.ibb.co/jgfY1SH/slider2.webp" alt="" />
     </div>
-    <div className=" bg-teal-700 h-[500px]">
-      <h3>Hello house crafters</h3>
+    <div className=" bg-teal-700 md:max-h-[83.5vh]">
+        <img className='w-full' src="https://i.ibb.co/mhR7pdj/slider3.webp" alt="" />
     </div>
   </Carousel>
 );
 
 export default HomeBanner;
+
