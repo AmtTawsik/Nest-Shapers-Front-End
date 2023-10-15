63aedac8aa6c72c7649167c39e1db6313fb027a6
AmtTawsik <amttawsik.cse@gmail.com>
2023-10-15 13:55:18 +0600

    Change Some Style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <PublicHeader />
-      <h1 className=" bg-red-500">Full Stack Final assignment</h1>
+      <h1 className=" bg-green-500">Full Stack Final assignment</h1>
     </main>
   );
 }
