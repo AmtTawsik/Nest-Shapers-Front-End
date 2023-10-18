1a3e41e42d30a9e3ea1ec667fb699f5a7c5a339f
AmtTawsik <amttawsik.cse@gmail.com>
2023-10-18 21:49:56 +0600

    Add Filter in Available Sirvices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <main>
       <div className=" bg-teal-700 text-center text-sm py-2 px-2 text-white">
-        Protect and Decorate Your Home With our Best Nest Shapers
+        Protect and Decorate Your Home With our Best Nest Shapers 
       </div>
       <PublicHeader />
       <HomeBanner />
