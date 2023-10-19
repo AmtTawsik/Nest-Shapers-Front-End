3f188503c12069b70aa1df9f8523c7fbac7eab97
AmtTawsik <amttawsik.cse@gmail.com>
2023-10-19 21:20:55 +0600

    Some Update in Home Function

--- src/app/(customer)/book-service/[serviceId]/page.tsx
+++
@@ -1,9 +0,0 @@
-function BookServiceSinglePage() {
-  return (
-    <div>
-      <h1>This is book service single page</h1>
-    </div>
-  );
-}
-
-export default BookServiceSinglePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-import HomeBanner from '@/components/homePage/Banner';
-import CallToAction from '@/components/homePage/CallToAction';
-import Categories from '@/components/homePage/Categories';
-import Features from '@/components/homePage/Features';
-import LatestBlogs from '@/components/homePage/LatestBlogs';
-import Overview from '@/components/homePage/Overview';
-import Reviews from '@/components/homePage/Reviews';
-import Services from '@/components/homePage/Services';
-import Showcase from '@/components/homePage/Showcase';
-import UpcomingService from '@/components/homePage/UpcomingService';
-import PublicHeader from '@/components/view/headers/PublicHeader';
-
-export default function Home() {
+function AdminlistPage() {
   return (
-    <main>
-      <div className=" bg-teal-700 text-center text-sm py-2 px-2 text-white">
-        Protect and Decorate Your Home With our Best Nest Shapers
-      </div>
-      <PublicHeader />
-      <HomeBanner />
-      <Overview />
-      <Features />
-      <Categories />
-      <Services />
-      <CallToAction />
-      <UpcomingService />
-      <Reviews />
-      <LatestBlogs />
-      <Showcase />
-    </main>
+    <div>
+      <h1>This is admin list page</h1>
+    </div>
   );
 }
+
+export default AdminlistPage;
